70faf040b295843f08addb875ecfded3ee2efd83
Erickx00 <erickdeveloperx10@gmail.com>
2026-07-21 16:29:53 -0300

    adding repository structure

---
+++ src/main/java/com/dio/spring/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.dio.spring.repository;
+
+import com.dio.spring.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
---
+++ src/main/java/com/dio/spring/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.dio.spring.repository;
+
+import com.dio.spring.model.Endereco;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
