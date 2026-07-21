d10aaa07f5885e6cdaab2e83a61b91d755dfb72e
Erickx00 <erickdeveloperx10@gmail.com>
2026-07-21 17:24:41 -0300

    adding GlobalExceptionAdvice and using somes depedencies

--- src/main/java/com/dio/spring/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dio.spring.repository;
-
-import com.dio.spring.model.Endereco;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends CrudRepository<Endereco, String> {
-}
